118d2a90aed1519015c58dc69af4191f9f865f80
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-11-14 15:20:55 -0300

    feat: add partials

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request=request, template_name='recipes/home.html', context={
-        "name": "John Doe"
-    })
+    return render(
+        request=request,
+        template_name='recipes/pages/home.html',
+        context={"name": "John Doe"}
+    )
